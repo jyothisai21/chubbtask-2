535ff3f53e478a3da887ed5649c00a335154fa21
jyothi sai <jyothisai709@gmail.com>
2021-06-10 23:44:04 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "Dashboard",
+    component : DashboardComponent
+  },
+  {
+    path : "user",
+    component : UserComponent
+  },
+  {
+    path : "product/:id",
+    component : ProductComponent
+  },  
+  {
+    path : "products",
+    component : ProductComponent
+  },  
+  {
+    path : "user/:id",
+    component : UserComponent
+  },  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jyothi'`, () => {
+  it(`should have as title 'routingDemo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jyothi');
+    expect(app.title).toEqual('routingDemo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jyothi app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('routingDemo app is running!');
   });
 });
